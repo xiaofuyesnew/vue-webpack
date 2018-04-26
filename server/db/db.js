4b16ad7c709f0f4a6cc557821f2375b92f36ae7e
xiaofuyesnew <xiaofuyesnew@qq.com>
2018-04-26 12:02:44 +0800

    db finish

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize')
 const config = require('./config.js')
 
-const sequelize = new Sequelize(config.database, 
+const sequelize = new Sequelize(
+  config.database, 
   config.username, 
   config.password, 
   {
@@ -15,4 +16,38 @@ const sequelize = new Sequelize(config.database,
   }
 )
 
-const Pet = sequelize.define('pet').
+const Pet = sequelize.define('pet', {
+  id: {
+    type: Sequelize.INTEGER(10),
+    primaryKey: true
+  },
+  name: Sequelize.STRING(100),
+  type: Sequelize.STRING(100),
+  weight: Sequelize.FLOAT(4, 2)
+}, {
+  timestamps: false
+})
+
+//增加数据
+/** *
+!(async () => {
+  const cat = await Pet.create({
+    id: 5,
+    name: '毛毛',
+    type: '兔子',
+    weight: 4.18
+  })
+  return console.log(`创建了${JSON.stringify(cat)}`)
+})()
+/** */
+
+!(async () => {
+  const pets = await Pet.findAll({
+    where: {
+      id: 3
+    }
+  })
+  for (let p of pets) {
+    console.log(JSON.stringify(p))
+  }
+})()
